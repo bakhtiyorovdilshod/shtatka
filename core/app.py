7a5d36e48036e0a690fa4f37390d2738045576e8
Dilshod Bakhtiyorov <baxtiyorovdilshod305@mail.ru>
2022-09-18 13:09:36 +0500

    fix

--- core/app.py
+++ core/app.py
@@ -6,8 +6,17 @@ from api.department.endpoints.settings import router as settings_router
 from api.client.endpoints.document import router as document_router
 from fastapi.middleware.cors import CORSMiddleware
 from core.settings import settings, database
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 app.include_router(prefix='/api/v1', router=user_router)
 app.include_router(prefix='/api/v1/shtat', router=department_router)
 app.include_router(prefix='/api/v1/shtat/department/settings', router=settings_router)
